7cee7ad8962482c9ae7f93957625668038cd587f
bonlin1999-bon <bonlin1999@gmail.com>
2026-05-04 14:53:54 +0800

    chore: rename PUBLIC_ROOM_SECRET to GLOBAL_ROOM_SECRET to avoid vercel warnings

--- api/config.js
+++ api/config.js
@@ -4,6 +4,6 @@ export default function handler(req, res) {
   res.status(200).json({
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
-    publicSecret: process.env.PUBLIC_ROOM_SECRET || '',
+    publicSecret: process.env.GLOBAL_ROOM_SECRET || '',
   });
 }
